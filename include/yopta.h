832bac9cfb284b4998290c1befa1b515aebb4da1
jnngl <jnngles@gmail.com>
2022-08-27 22:55:16 +1100

    йопта стринги и т.д.

---
+++ include/yopta.h
@@ -0,0 +1,127 @@
+#pragma once
+
+#ifndef __cplusplus
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#else
+#include <cstdint>
+#include <cstdio>
+#endif
+
+#define харе break
+#define лещ case
+#define аеслинайду case
+#define аченетак catch
+#define аченитак catch
+#define ачёнетак catch
+#define двигай continue
+#define ёбнуть delete
+#define ебнуть delete
+#define крч do
+#define иливжопураз else
+#define го for
+#define уебать ~
+#define вилкойвглаз if
+#define пох ::
+#define пахану default
+#define апохуй default
+#define наотыбись default
+#define гыйбать new
+#define захуярить new
+#define отвечаю return
+#define естьчо switch
+#define тырыпыры this
+#define пнх throw
+#define хапнуть try
+#define побратски try
+#define побрацки try
+#define пабрацки try
+#define пабратски try
+#define гыы auto
+#define куку void
+#define потрещим while
+#define говнойбать virtual
+#define пацан bool
+#define семка unsigned char
+#define эээ char
+#define клёво class
+#define клево class
+#define ясенхуй const
+#define двойные double
+#define еээ enum
+#define батя :
+#define бачок const
+#define плавник float
+#define пиздуй goto
+#define силикон override
+#define спиздить #include
+#define хуйня int
+#define колонна long
+#define писькомер size_t
+#define подкрыша protected
+#define чоунастут template
+#define мой private
+#define ебанное public
+#define пипин short
+#define попонятия static
+#define вписос volatile
+#ifndef __cplusplus
+#define нуллио NULL
+#else
+#define нуллио nullptr
+#endif
+#define порожняк нуллио
+#define нихуя 0
+#define неибу 0
+#define трулио true
+#define чётко true
+#define четко true
+#define чотко true
+#define нетрулио false
+#define пиздишь false
+#define нечётко false
+#define нечетко false
+#define нечотко false
+#define ятеотвечаю
+#define отжать &
+#define нихуясебе *
+#define жЫ {
+#define есть }
+#define эквалио ==
+#define однахуйня ==
+#define типа ==
+#define блясука ==
+#define блябуду ==
+#define чёткоровно ==
+#define четкоровно ==
+#define чоткоровно ==
+#define конкретно ==
+#define поцик >=
+#define поц <=
+#define ичо &&
+#define иличо ||
+#define пизже >
+#define хуёвей <
+#define хуевей <
+#define сука =
+#define внатуре =
+#define чобля !
+#define чоблясука !=
+#define нах ;
+#define нахуй ;
+#define бля ;
+#define плюсуюна ++
+#define слилсяна --
+
+#define малява printf
+#define напиздеть fprintf
+#define шухер stderr
+#define ксива stdout
+
+#define найтиЛоха *
+
+#define наебать noreturn
+#define поясниЗаБазар nodiscard
+
+#include "yopta/string.h"
---
+++ include/yoptac.h
@@ -0,0 +1,4 @@
+#pragma once
+
+#include <yopta/string.h>
+#include <yopta.h>
